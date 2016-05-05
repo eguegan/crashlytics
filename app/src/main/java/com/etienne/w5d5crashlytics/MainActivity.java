4fa00e6dd920ac9f88776f8a0cfdc47645829e0a
eguegan <e.guegan@margulls.com>
2016-05-05 17:01:56 -0400

    add crash

--- app/src/main/java/com/etienne/w5d5crashlytics/MainActivity.java
+++ app/src/main/java/com/etienne/w5d5crashlytics/MainActivity.java
@@ -1,13 +1,20 @@
 package com.etienne.w5d5crashlytics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

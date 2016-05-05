85c4f9197c620e20eecdcb64b4e59c83cb691d62
eguegan <e.guegan@margulls.com>
2016-05-05 17:24:53 -0400

    add answer with fabric

--- app/src/main/java/com/etienne/w5d5crashlytics/MainActivity.java
+++ app/src/main/java/com/etienne/w5d5crashlytics/MainActivity.java
@@ -3,6 +3,8 @@ package com.etienne.w5d5crashlytics;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
@@ -11,10 +13,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+//        Fabric.with(this, new Crashlytics());
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Crashlytics())
+                .debuggable(true)
+                .build();
+        Fabric.with(fabric);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        Log.d("TAG", "onCreate: " + Fabric.isDebuggable());
+
+//        throw new RuntimeException("Crashhh");
+
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
     }
 }
